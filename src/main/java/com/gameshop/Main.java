e5c96c6c887c832e847c6ffda09091e87370f74d
Manuel Fuentes <mfuecru1804@g.educaand.es>
2026-02-18 10:33:06 +0100

    Fix: Refactorizacion de GestorInventario. Closes#3

--- src/main/java/com/gameshop/Main.java
+++ src/main/java/com/gameshop/Main.java
@@ -12,7 +12,6 @@ public class Main {
         System.out.println("--- 1. TEST DE INVENTARIO ---");
         String[] juegos = {"Cyberpunk", "Zelda", "Mario Kart", "Halo"};
         int[] stock = {10, 2, 15, 4};
-        // BUG: El programa nunca avisará del stock bajo de 'Halo' ni lo sumará al total.
         inventario.p(juegos, stock);
 
         // --- PRUEBA ALUMNO 2: VENTAS ---
